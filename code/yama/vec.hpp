17deecfe5f976ab9090c2ff1c317fdc4cd0c8044
Borislav Stanimirov <b@ibob.bg>
2026-07-18 20:17:38 +0300

    not sure

---
+++ code/yama/vec.hpp
@@ -0,0 +1,13 @@
+// Copyright(c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+
+namespace yama {
+
+template <typename T, size_t N>
+struct vec {
+
+};
+
+} // namespace yama
